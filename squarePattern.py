f30d92ae48f0667cf2fad8e3955d7677b7f908fc
Menyichel <dawabeiz10@gmail.com>
2023-01-02 23:57:16 +0300

    Square pattern

---
+++ squarePattern.py
@@ -0,0 +1,8 @@
+#square pattern using for loop
+x=int(input("Enter the number of rows and columns of square: "))
+
+for a in range(0,x):
+    for b in range(0,x):
+        print(end="# ")
+    print(end="\n")
+    
